fcb1efe74bb44d6c44a9063fff1838d9172750e4
Sascha <sascha@landwire.net>
2016-02-14 11:47:38 +0100

    push

--- relayer.js
+++ relayer.js
@@ -1,3 +1,5 @@
+zeus = require('./zeus.js');
+
 var getMedianTemperature = function(data) {
 	var tot=data.length;
 	var median=0;
@@ -6,25 +8,6 @@ var getMedianTemperature = function(data) {
 	}
 	median = median/tot;
 	return median;
-}
-
-var datas = [20,20,20,25,25,20,20,20,20,20,20,23];
-console.log(getMedianTemperature(datas));
-
-var alarm = false;
-var medianTemp;
-while (alarm === false) {
-    medianTemp=getMedianTemperature(datas);
-    datas.pop();
-    datas.unshift(60); // 90 is new temperature reading
-    console.log(medianTemp);
-    if(medianTemp > 50) {
-    	alarm=true;
-    }
-    // need to onstruct that in another way!!!
-    setTimeout(function() {
-    	console.log('Waiting 1 second...');
-    }, 1000);
 }
 
 // RELAYR.IO STUFF
@@ -51,7 +34,8 @@ while (alarm === false) {
 	relayr.connect(token, dev_id);
 	//console.log('Connected');
 
-	var temperature;
+	var last_temperatures=[20,20,20,25,25,20,20,20,20,20];
+	var averageTemperature = 20;
 	relayr.on("data", function (topic, msg) {
 		console.log('TOPIC');
 	    console.log(topic);
@@ -59,15 +43,48 @@ while (alarm === false) {
 	    console.log(msg);
 	    // the temperature
 	    console.log( msg.readings[0].value);
-	    
+	    console.log( msg.deviceId);
+
+	    // check for freak readings
+	    // get the average temperature
+    	averageTemperature = getMedianTemperature(last_temperatures);
+    	if(msg.readings[0].value > (averageTemperature + 50) ) {
+    		// it's a freak reading, do nothing!!!
+    	} else {
+    		// track last 10 readings = 30 minute;
+ 	    	last_temperatures.pop();
+    		last_temperatures.unshift(msg.readings[0].value); // 90 is new temperature reading
+    	}
+
+    	console.log(last_temperatures);
+
+    	// get the average temperature
+    	averageTemperature = getMedianTemperature(last_temperatures);
+
+    	// check for heat
+    	if(averageTemperature > 50) {
+    		console.log('HOT HOT HOT');
+    	} else {
+    		console.log('All COOL');
+    	}
+    	var test3 = {
+    	"name": "Jackson Bonde",
+		"device": "9af8642a-65e1-4b9b-b255-515268ae38b5",
+		"temperature": msg.readings[0].value,
+		"coordinates": "52°28'52.9\"N 13°21'25.2\"E",
+		"address": "EUREF-Campus, 10829 Berlin",
+		"phone": "+4915775983808"
+		}
+	    //sendDataToZeus();
+	    zeus.send(test3)
 	});
 
 	relayr.on("connect", function () {
 	 console.log("Connected");
 	});
 
-	console.log('STUPID TEMPERATURE');
-	console.log(temperature);
+	//console.log('STUPID TEMPERATURE');
+	//console.log(temperature);
 
 	
 
