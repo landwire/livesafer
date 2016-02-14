01f0545c0022a7e2d902a303f0b7150384902007
Sascha <sascha@landwire.net>
2016-02-14 13:13:50 +0100

    last minute

--- index.js
+++ index.js
@@ -1,72 +1,132 @@
-var express = require('express');
-var app = express();
-var fs = require("fs");
-var obj;
+zeus = require('./zeus.js');
+request = require('superagent');
+
+request
+    .post('https://api.tropo.com/1.0/sessions')
+    .send("action=create")
+    .send("token=5a4168777877505150566d51764652554f6f6161494950624d72635a4456534159496d69454d4d487649534c")
+    .send("customerTelephone=openberlin3.gen@cisco.com")
+    .send("customerName=John+Dyer")
+    .send("neighbourTelephone=openberlin3.gen@cisco.com")
+    .send("neighbourName=Jane+Dyer")
+    	if (err || !res.ok) {
+    		console.log('TROPo error: ', err);
+    	} else {
+    		console.log('Call initiated:');
+   		}
+   	});
 
 
-app.get('/', function (req, res) {
-	var response = 'Welcome to Lyfee Safer';
-	res.send(response);
-});
+var getMedianTemperature = function(data) {
+	var tot=data.length;
+	var median=0;
+	for (var i=0; i < tot; i++) {
+	  median = median + data[i];
+	}
+	median = median/tot;
+	return median;
+}
 
-app.get('/test', function (req, res) {
-  	var response = 'TEST!!!';
-	res.send(response);
-});
+// RELAYR.IO STUFF
+	var Relayr = require('relayr');
+	var app_id = "{ebe9f40a-f6df-4760-8c43-1c8a35fd50f8}";
+	var dev_id = "9af8642a-65e1-4b9b-b255-515268ae38b5";
+	var token  = "VFxQi3GrH1R-FxXf8jcRJ-lQlN1N3xuA";
 
-app.get('/monitor', function (req, res) {
-  	// Asynchronous read
-  	/*
-  	fs.readFile('input.txt', function (err, data) {
-  	   	if (err) {
-  	       return console.error(err);
-  	   	}
-  	   	//make data JSON
-  	   	obj = JSON.parse(data);
-  	   	// data is just text here!!!
-  	   	res.send("Asynchronous read: " + data);
-  	});*/
 	
+
+
 	
-  	// relayr
-  	var Relayr = require('relayr');
-  	var app_id = "{ebe9f40a-f6df-4760-8c43-1c8a35fd50f8}";
-  	var dev_id = "9af8642a-65e1-4b9b-b255-515268ae38b5";
-  	var token  = "VFxQi3GrH1R-FxXf8jcRJ-lQlN1N3xuA";
 
-  	var relayr = new Relayr(app_id);
+	var relayr = new Relayr(app_id);
+	console.log('START');
+	/*
+	relayr.deviceModel(token, dev_id, function (err, description) {
+	    console.log("-------------- BSH --------------------");
+	    console.log(description);
+	    console.log("----------------------------------------------------------");
+	});
+	*/
 
-  	/*
-  	relayr.deviceModel(token, dev_id, function (err, description) {
-  	    console.log("-------------- BSH --------------------");
-  	    console.log(description);
-  	    console.log("----------------------------------------------------------");
-  	});
-  	*/
+	relayr.connect(token, dev_id);
+	//console.log('Connected');
 
-  	relayr.connect(token, dev_id);
-  	/*
-  	relayr.on("data", function (topic, msg) {
-  	    console.log(topic + ":" + msg);
-  	});
-  	*/
-  	relayr.on("connect", function () {
-  	 console.log("connected");
-  	});
+	var last_temperatures=[20,20,20,25,25,20,20,20,20,20];
+	var averageTemperature = 20;
+	relayr.on("data", function (topic, msg) {
+		console.log('TOPIC');
+	    console.log(topic);
+	    console.log('msg');
+	    console.log(msg);
+	    // the temperature
+	    console.log( msg.readings[0].value);
 
-  	setTimeout(function() {
-  	    console.log('Waited 3 seconds');
-  	}, 3000);
+	    console.log( msg.deviceId);
 
-});
+	    // check for freak readings
+	    // get the average temperature
+    	averageTemperature = getMedianTemperature(last_temperatures);
+    	if( (msg.readings[0].value > (averageTemperature + 50)) || (msg.readings[0].value < (averageTemperature - 50)) ) {
+    		// it's a freak reading, do nothing!!!
+    	} else {
+    		// track last 10 readings = 30 minute;
+ 	    	last_temperatures.pop();
+    		last_temperatures.unshift(msg.readings[0].value); // 90 is new temperature reading
+    	}
 
+    	console.log(last_temperatures);
 
+    	// get the average temperature
+    	averageTemperature = getMedianTemperature(last_temperatures);
 
-var server = app.listen(3000, function () {
+    	// check for heat
+    	if(averageTemperature > 50) {
+    		console.log('HOT HOT HOT');
+    		request
+    		  .post('https://api.tropo.com/1.0/sessions?action=create')
+    		  .send("token=5a4168777877505150566d51764652554f6f6161494950624d72635a4456534159496d69454d4d487649534c")
+    		  .send("customerTelephone=14075551212")
+    		  .send("customerName=John+Dyer")
+    		  .send("neighbourTelephone=123132123123")
+    		  .send("neighbourName=Jane+Dyer")
+    		  .end(function(err, res){
+    		    // Calling the end function will send the request
+    		});
+    	} else {
+    		console.log('All COOL');
+    	}
+    	var test3 = {
+    	"name": "Jackson Bonde",
+		"device": "9af8642a-65e1-4b9b-b255-515268ae38b5",
+		"temp2": parseInt(msg.readings[0].value),
+		"coordinates": "52.48202, 13.35713",
+		"address": "EUREF-Campus, 10829 Berlin",
+		"phone": "+4915775983808"
+		}
+	    //sendDataToZeus();
+	    zeus.send(test3);
+	    //zeus.send(30);
+	});
 
-    var host = server.address().address;
-    var port = server.address().port;
+	relayr.on("connect", function () {
+	 console.log("Connected");
+	});
 
-    console.log('Example app listening at http://%s:%s', host, port);
+	//console.log('STUPID TEMPERATURE');
+	//console.log(temperature);
 
-});
+	
+
+var getDeviceTemperature = function(msg) {
+	console.log('MSG');
+	console.log(msg);
+	if (typeof(msg.readings) != "undefined") {
+		return msg.readings[0].value
+	} else {
+		// error handling
+		return 'ERROR';
+	}
+}
+
+//var test = getDeviceTemperature();
+console.log('TEST');
